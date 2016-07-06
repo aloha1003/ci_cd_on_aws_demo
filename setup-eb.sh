54128d4868de059fc59400297c4609db77a0e002
john <john_lin@mypay.tw>
2016-07-06 09:50:15 +0800

    修正eb init設定

--- setup-eb.sh
+++ setup-eb.sh
@@ -2,18 +2,18 @@
 
 set -x
 set -e
-if [ ! -d "/home/ubuntu/.aws" ]; then
-    mkdir /home/ubuntu/.aws
-    echo 'In'
-  # Control will enter here if $DIRECTORY exists.
-fi
-if [  -f "/home/ubuntu/.aws/config" ]; then
-    rm /home/ubuntu/.aws/config
-    echo 'In'
-fi
-touch chmod 600 /home/ubuntu/.aws/config
-echo "[profile eb-cli]" > /home/ubuntu/.aws/config
-echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
-echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
+# if [ ! -d "/home/ubuntu/.aws" ]; then
+#     mkdir /home/ubuntu/.aws
+#   # Control will enter here if $DIRECTORY exists.
+# fi
+# if [  -f "/home/ubuntu/.aws/config" ]; then
+#     rm /home/ubuntu/.aws/config
+    
+# fi
+mv elasticbeanstalk-config.yml .elasticbeanstalk/config.yml
+# touch chmod 600 /home/ubuntu/.aws/config
+# echo "[profile eb-cli]" > /home/ubuntu/.aws/config
+# echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
+# echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
  
 
