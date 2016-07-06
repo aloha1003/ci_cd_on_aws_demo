bfabedcb2bb1940362882b89a08a5a7a117b36e9
john <john_lin@mypay.tw>
2016-07-06 14:55:53 +0800

    修改錯誤測試

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
 // test/test.js
+// test
 var assert = require("assert")
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(0));
+      assert.equal(-12, [1,2,3].indexOf(0));
     })
   })
 })
