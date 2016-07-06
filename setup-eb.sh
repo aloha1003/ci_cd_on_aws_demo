b5f35e021980b32770eda1e4dc3dd3803e07a023
john <john_lin@mypay.tw>
2016-07-06 09:35:09 +0800

    修正circle.yml設定檔

--- setup-eb.sh
+++ setup-eb.sh
@@ -1,9 +1,16 @@
+#!/bin/sh
+
 set -x
 set -e
+if [ ! -d "/home/ubuntu/.aws" ]; then
+    mkdir /home/ubuntu/.aws
+  # Control will enter here if $DIRECTORY exists.
+fi
+if [ ! -f "/home/ubuntu/.aws/config"]; then
+    touch chmod 600 /home/ubuntu/.aws/config
+    echo "[profile eb-cli]" > /home/ubuntu/.aws/config
+    echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
+    echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
+fi
+ 
 
-mkdir /home/ubuntu/.aws
-touch /home/ubuntu/.aws/config
-chmod 600 /home/ubuntu/.aws/config
-echo "[profile eb-cli]" > /home/ubuntu/.aws/config
-echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
-echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
