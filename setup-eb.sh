f91279dca7cfe4629f9c21b6a6c26e545518084f
john <john_lin@mypay.tw>
2016-07-06 10:17:12 +0800

    修正eb init設定

--- setup-eb.sh
+++ setup-eb.sh
@@ -2,17 +2,20 @@
 
 set -x
 set -e
-# if [ ! -d "/home/ubuntu/.aws" ]; then
-#     mkdir /home/ubuntu/.aws
-#   # Control will enter here if $DIRECTORY exists.
-# fi
-# if [  -f "/home/ubuntu/.aws/config" ]; then
-#     rm /home/ubuntu/.aws/config
-    
-# fi
+if [ ! -d "/home/ubuntu/.aws" ]; then
+    mkdir /home/ubuntu/.aws
+  # Control will enter here if $DIRECTORY exists.
+fi
+if [  -f "/home/ubuntu/.aws/config" ]; then
+    value=`cat /home/ubuntu/.aws/config`
+    echo "$value"
+fi
+
+
 if [ ! -d ".elasticbeanstalk" ]; then
     mkdir .elasticbeanstalk
 fi
 mv elasticbeanstalk-config.yml .elasticbeanstalk/config.yml
 
  
+
