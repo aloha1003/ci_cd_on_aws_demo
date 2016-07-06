ab66b30a906321bad04204d92c9bd49f6b3fc949
john <john_lin@mypay.tw>
2016-07-06 10:49:32 +0800

    更新設定

--- setup-eb.sh
+++ setup-eb.sh
@@ -10,10 +10,8 @@ fi
 echo "[profile eb-cli]" > /home/ubuntu/.aws/config
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
-if [ ! -d ".elasticbeanstalk" ]; then
-    mkdir .elasticbeanstalk
-fi
-mv elasticbeanstalk-config.yml .elasticbeanstalk/config.yml
+
+mv .elasticbeanstalk/config-global.yml .elasticbeanstalk/config.yml
 
  
 
